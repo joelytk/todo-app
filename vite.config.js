0a82695edfa9ccc758a0e00a79e652357f54d55d
Joel Yap <joelytk@gmail.com>
2023-04-27 01:05:00 +0800

    Deployed to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,8 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'build'
   }
 });
